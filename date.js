14920830ca1dfe9ca3782843fc4bf45c0c166d76
akashdagur3333 <KnocialIndiaLimited>
2023-08-15 03:21:41 +0530

    a

--- date.js
+++ date.js
@@ -1,6 +1,8 @@
 
  var newDate=new Date()
  
+ newDate.setHours(newDate.getHours()+5)
+ newDate.setMinutes(newDate.getMinutes()+30)
 //  let date = ("0" + newDate.getDate()).slice(-2);
 
 //  // current month
